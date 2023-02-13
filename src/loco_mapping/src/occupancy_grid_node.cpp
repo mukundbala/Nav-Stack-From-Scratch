290a08b87750be24c4baf566acc21512e7a6f63f
Mukund Bala <mukund199823@gmail.com>
2023-02-14 01:12:04 +0800

    occupancy grid tested and complete

--- src/loco_mapping/src/occupancy_grid_node.cpp
+++ src/loco_mapping/src/occupancy_grid_node.cpp
@@ -1,6 +1,10 @@
+#include "occupancy_grid.h"
 
-
-int main()
+int main(int argc, char** argv)
 {
-    
+    ros::init(argc , argv , "occupancy_grid_node");
+    ros::NodeHandle nh;
+    OccupancyGrid og(nh);
+    og.run();
+    return 0;
 }
