a0e9fcceb2d53bd02b1545e4f54b4f8bcede8dc2
mukundbala <mukund199823@gmail.com>
2023-03-07 11:51:53 +0800

    added proj 2 option for params.sh file

--- params.sh
+++ params.sh
@@ -1,2 +1,2 @@
-export WORLD="world21" ##options: world20 world21 world23proj1. ##Note that world23proj1 will not create a gazebo, cus theres no sim world
-export TASK="labs" ##options labs proj1
+export WORLD="world20" ##options: world20 world21 world23proj1. ##Note that world23proj1 will not create a gazebo, cus theres no sim world
+export TASK="proj2" ##options: labs proj1 proj2
