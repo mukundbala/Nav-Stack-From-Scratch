6cad7bc33d512edc68083ef0bec357dd99186576
mukundbala <mukund199823@gmail.com>
2023-03-18 19:52:22 +0800

    easy to revert between solo and coop

--- params.sh
+++ params.sh
@@ -1,2 +1,2 @@
-export WORLD="world0" ##options: world20 world21 world23proj1 world0. ##Note that world23proj1 will not create a gazebo, cus theres no sim world. Note that world0 is for tuning
-export TASK="soloflight" ##options: labs proj1 proj2 soloflight
+export WORLD="world20" ##options: world20 world21 world23proj1 world0. ##Note that world23proj1 will not create a gazebo, cus theres no sim world. Note that world0 is for tuning
+export TASK="proj2" ##options: labs proj1 proj2 soloflight
--- run.sh
+++ run.sh
@@ -20,12 +20,12 @@ sleep 1s # allow gazebo to reset so as not to mess with the time.
 
 # bring up gazebo and build all models
 echo "Begin..."
+
+if [ $TASK = "soloflight" ];
+then 
+roslaunch turtle_bringup soloflight.launch
+else 
 roslaunch turtle_bringup run_client.launch
-
-# try to stop. This will show an error if ./bringup.launch is not running
 echo "Stopping Turtlebot3 by publishing to /turtle/cmd_vel"
 rostopic pub -1 /turtle/cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
-
-# try to stop. This will show an error if ./bringup.launch is not running
-echo "Stopping Turtlebot3 by publishing to /turtle/cmd_vel"
-rostopic pub -1 /turtle/cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+fi
