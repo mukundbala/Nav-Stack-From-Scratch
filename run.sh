a3c67953cbecd72402015a622ae9bb57be8e3406
mukundbala <mukund199823@gmail.com>
2023-04-22 17:11:28 +0800

    aligned changes to launch files

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ source params.sh
 source devel/setup.bash
 
 # get the world specific parameters as environment variables
-source `rospack find turtle_bringup`/worlds/`echo $WORLD`/`echo $WORLD`.sh
+source `rospack find robot_bringup`/worlds/`echo $WORLD`/`echo $WORLD`.sh
 
 # reset the gazebo
 echo "Resetting Gazebo. Wait 1s for Gazebo to complete reset."
@@ -20,12 +20,18 @@ sleep 1s # allow gazebo to reset so as not to mess with the time.
 
 # bring up gazebo and build all models
 echo "Begin..."
-
-if [ $TASK == "soloflight" ];
-then 
-roslaunch turtle_bringup soloflight.launch
-else 
-roslaunch turtle_bringup run_client.launch
+roslaunch robot_bringup run_robots.launch
+if [ $TASK == "mode1" ];then
 echo "Stopping Turtlebot3 by publishing to /turtle/cmd_vel"
 rostopic pub -1 /turtle/cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+elif [ $TASK == "mode2" ];then
+echo "Stopping Hector by setting disabling motors!"
+rostopic pub -1 /hector/cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+rosservice call /hector/enable_motors "enable: false" 
+else
+echo "Stopping Turtlebot3 by publishing to /turtle/cmd_vel"
+rostopic pub -1 /turtle/cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+echo "Stopping Hector by disabling motors!"
+rostopic pub -1 /hector/cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+rosservice call /hector/enable_motors "enable: false" 
 fi
